82b5bac2e272362aae257e1c6fa7d9558dbab908
Princy0204 <princypandey241@gmail.com>
2023-12-09 01:19:03 +0530

    princyy

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgO-zeYZXYnUU42c1vAAyMzvDzG989wbI",
+  authDomain: "realtor-3811c.firebaseapp.com",
+  projectId: "realtor-3811c",
+  storageBucket: "realtor-3811c.appspot.com",
+  messagingSenderId: "1766873961",
+  appId: "1:1766873961:web:b0c0b28c4ecd37257a7bf8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
